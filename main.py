7730cc0a125f28b45954dde4a0aae24fd9a0f2e5
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-04-15 23:16:19 -0400

    added padding and selects files

--- main.py
+++ main.py
@@ -18,15 +18,27 @@ class Main():
         frame = Frame(self.mainFrame)
         Label(frame,text = 'Image').grid(sticky=W)
         Entry(frame,textvariable = self.image,width=50).grid(row=0,column=1,padx=10,pady=10)
+        Button(frame,text='Browse',command=self.browse).grid(row=0,column=2,pady=10,padx=10)
         Label(frame,text = 'Grid').grid(sticky=W)
         OptionMenu(frame,self.grid,*[3,4,5,6,7,8,9,10]).grid(row=1,column=1,padx=10,pady=10)
-        frame.pack()
+        frame.pack(padx=10,pady=10)
+        Button(self.mainFrame,text='Start',command=self.start).pack(padx=10,pady=10)
         self.mainFrame.pack()
         self.board = Frame(self.parent)
         self.winframe = Frame(self.parent)
+
+    def start(self):
+        image = self.image.get()
+        grid = self.grid.get()
+        if os.path.exists(image):
+            self.board=Board()
+
+    def browse(self):
+        self.image.set(tkFileDialog.askopenfilename(title='Select Image',filetype = ( ('png File','*.png'),('jpg File','*.jpg') )) )
 
 
 if __name__=="__main__":
     root = Tk()
     Main(root)
+    print('test1')
     root.mainloop()
